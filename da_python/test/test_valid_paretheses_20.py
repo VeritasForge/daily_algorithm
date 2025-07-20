5b9a7bb9f21da0bd7160b84b8628f5ab5553b58c
VeritasForge <greenfrog@outlook.kr>
2025-07-20 10:14:19 +0900

    complete: 20.Valid Paretheses

---
+++ da_python/test/test_valid_paretheses_20.py
@@ -0,0 +1,17 @@
+import pytest
+
+from src.leetcode.valid_paretheses_20 import is_valid
+
+
+@pytest.mark.parametrize(
+    "s, expected",
+    [
+        ("()", True),
+        ("()[]{}", True),
+        ("([])", True),
+        ("(]", False),
+        ("([)]", False),
+    ],
+)
+def test_is_valid(s, expected):
+    assert is_valid(s) is expected
