6f5208f62cb3ebb0c1b0619da1f25a2c8a7c7a86
VeritasForge <greenfrog@outlook.kr>
2025-07-26 17:13:41 +0900

    refactor(27): fixed a specific failing test case

--- da_python/test/leetcode/test_remove_element_27.py
+++ da_python/test/leetcode/test_remove_element_27.py
@@ -10,6 +10,9 @@ from src.leetcode.remove_element_27 import remove_element
         ([2, 2, 3, 3], 3, [2, 2]),
         ([3, 3, 2, 2], 3, [2, 2]),
         ([0, 1, 2, 2, 3, 0, 4, 2], 2, [0, 1, 3, 0, 4]),
+        ([2], 3, [2]),
+        ([3], 3, []),
+        ([3, 3], 5, [3, 3]),
     ],
 )
 def test_remove_element(nums, val, expected):
