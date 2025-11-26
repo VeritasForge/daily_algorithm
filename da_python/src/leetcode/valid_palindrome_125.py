04843d87af9e199920ba39c8f0ecd1770b60b05c
VeritasForge <greenfrog@outlook.kr>
2025-11-27 07:42:50 +0900

    [leetcode][easy] 125. Valid Palindrome

--- da_python/src/leetcode/valid_palindrome_125.py
+++ da_python/src/leetcode/valid_palindrome_125.py
@@ -32,6 +32,30 @@
 # s consists only of printable ASCII characters.
 
 
+import re
+
+
+# pythonic way
 def is_palindrome(s: str) -> bool:
+    # return _pythonic(s)
+    return _using_two_point(s)
+
+
+def _pythonic(s: str) -> bool:
     filtered_str = "".join(c for c in s.lower() if c.isalnum())
     return filtered_str == filtered_str[::-1]
+
+
+def _using_two_point(s: str) -> bool:
+    # filtered_str = "".join(c for c in s.lower() if c.isalnum())
+    filtered_str = re.sub("[^0-9a-zA-Z]", "", s).lower()
+    left, right = 0, len(filtered_str) - 1
+
+    for _ in range(len(filtered_str) // 2):
+        if filtered_str[left] != filtered_str[right]:
+            return False
+
+        left += 1
+        right -= 1
+
+    return True
